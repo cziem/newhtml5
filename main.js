5ad892e66dde79e285fe3cd42ed116e05cb6aa7e
Fay Vor <phavorsparks@gmail.com>
2018-05-03 10:56:13 +0100

    Draw good

--- main.js
+++ main.js
@@ -1,2 +1,15 @@
 
-var 
+var canvas = document.getElementById('drawingsurface');
+var ctx = canvas.getContext('2d');
+
+// draw diagonal line
+ctx.beginPath();
+ctx.moveTo(100, 150);
+ctx.lineTo(75, 100);
+ctx.moveTo(100, 150);
+ctx.lineTo(450, 50);
+ctx.lineWidth = 10;
+
+// set color
+ctx.strokeStyle = '#ff0000';
+ctx.stroke();
