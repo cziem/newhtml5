7f91bb1c51e6914beaae5c2675d57ad851c6f0aa
Fay Vor <phavorsparks@gmail.com>
2018-05-03 11:10:15 +0100

    Draw rectangle

--- main.js
+++ main.js
@@ -3,13 +3,22 @@ var canvas = document.getElementById('drawingsurface');
 var ctx = canvas.getContext('2d');
 
 // draw diagonal line
-ctx.beginPath();
-ctx.moveTo(100, 150);
-ctx.lineTo(75, 100);
-ctx.moveTo(100, 150);
-ctx.lineTo(450, 50);
-ctx.lineWidth = 10;
+// ctx.beginPath();
+// ctx.moveTo(100, 150);
+// ctx.lineTo(75, 100);
+// ctx.moveTo(100, 150);
+// ctx.lineTo(450, 50);
+// ctx.lineWidth = 10;
 
-// set color
-ctx.strokeStyle = '#ff0000';
+// // set color
+// ctx.strokeStyle = '#ff0000';
+// ctx.stroke();
+
+// draw a rectangle
+ctx.beginPath();
+ctx.rect(188, 50, 200, 100);
+ctx.fillStyle = 'yellow';
+ctx.fill();
+ctx.lineWidth = 7;
+ctx.strokeStyle = 'black';
 ctx.stroke();
