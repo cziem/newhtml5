2f3c5f604abf9c96de80d4f7ce300fe770da89c5
Fay Vor <phavorsparks@gmail.com>
2018-05-04 10:47:55 +0100

    Add Js functionality

---
+++ drag.js
@@ -0,0 +1,17 @@
+
+function allowDrop(ev) {
+    ev.preventDefault();
+}
+
+function drag(ev) {
+    ev.dataTransfer.setData('text', ev.target.id);
+}
+
+function drop(ev) {
+    ev.preventDefault();
+    var data = ev.dataTransfer.getData('text');
+    var dropped = ev.target.appendChild(document.getElementById(data));
+    if (dropped) {
+        data.style.border = '5px solid blue';
+    }
+}
